37c2b8ab5f4e1f1b2a6427dfca4ba4a7855d9809
Abdul-Bakri <66844942+Bakri-10@users.noreply.github.com>
2023-10-01 22:47:34 +0100

    Update ec2.tf

--- ec2.tf
+++ ec2.tf
@@ -41,9 +41,12 @@ resource "aws_autoscaling_group" "sigmatek_asg" {
 
 # Create a new load balancer attachment
 resource "aws_autoscaling_attachment" "sigmatek_asg_at" {
-  autoscaling_group_name = aws_autoscaling_group.sigmatek_asg.id
-  elb                    = aws_lb.sigmatek_lb.id
+  autoscaling_group_name = aws_autoscaling_group.sigmatek_asg.name
+  lb_target_group_arn  = aws_lb_target_group.sigmatek_lb_tg.arn
+  depends_on            = [aws_autoscaling_group.sigmatek_asg, aws_lb_target_group.sigmatek_lb_tg]
 }
+
+
 
 ##################################
 #Application load balancer
